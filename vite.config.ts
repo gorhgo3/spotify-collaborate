0511bf330a2df6ed54a980eba361d614d504aab0
Murray Sinclair <msin981@gmail.com>
2023-12-09 17:33:25 +1300

    setup: Vite & TS setup alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@models': path.resolve(__dirname, './models'),
+      '@styles': path.resolve(__dirname, './client/styles'),
+      '@assets': path.resolve(__dirname, './client/assets'),
+      '@pages': path.resolve(__dirname, './client/pages'),
+      '@components': path.resolve(__dirname, './client/components'),
+    },
+  },
 })
