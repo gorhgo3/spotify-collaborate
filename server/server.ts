552292cdb0e55d1b89d35e370aa7d614bbec2272
Murray Sinclair <msin981@gmail.com>
2023-12-10 15:06:31 +1300

    feat: Express server routing and cors

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,17 @@
 import express from 'express'
+import Shadow from './routes/spotifyLogin'
+import cors from 'cors'
+import session from 'express-session'
 
 const server = express()
+
+server.use(cors())
+server.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' }
+}));
+server.use('/api/v1/server-master', Shadow)
 
 export default server
