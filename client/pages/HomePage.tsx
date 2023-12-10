1d11a5282a83da488ecf49b71279f48608467c02
Murray Sinclair <msin981@gmail.com>
2023-12-10 15:07:11 +1300

    format: HomePage includes App

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import App from '@components/App'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>HomePage</h1>
+      <App />
+    </div>
   )
 }
 
