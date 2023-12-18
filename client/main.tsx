752afd88e2c375ad574166a5ed67f1165877be94
Murray Sinclair <msin981@gmail.com>
2023-12-18 18:25:56 +1300

    feat: Auth0Provider & RouterProvider added main.tsx

--- client/main.tsx
+++ client/main.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomePage from '@pages/HomePage'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { router } from './routes/index.tsx'
+import Navbar from '@components/Navbar.tsx'
+
+const routes = createBrowserRouter(router)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage/>
+    <Auth0Provider
+      domain="manaia-2023-murray.au.auth0.com"
+      clientId="XkOBUlgkesz5PLbTkVYoytgfC1c8GYcb"
+      authorizationParams={{
+        redirect_uri: window.location.origin + "/home",
+      }}
+    >
+      <Navbar />
+      <RouterProvider router={routes} />
+    </Auth0Provider>
   </React.StrictMode>
 )
