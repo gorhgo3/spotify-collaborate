30157f020f83642203e6fd281e99014738a3bc75
Murray Sinclair <msin981@gmail.com>
2023-12-21 11:53:43 +1300

    feat: Routes spotifyPlaylists new playlist

---
+++ server/routes/spotifyPlaylists.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import Router from 'express'
+import shadowCredentials from '../shadow'
+
+const router = Router()
+
+// POST creates a public playlist
+router.post('/add-playlist', async (req, res) => {
+  try {
+    const playlistName = req.body.playlistName
+    const playlistDescription = req.body.playlistDescription
+    // need to find the user_id
+    const userId = shadowCredentials.user?.id
+    const addPlaylistRoute = `https://api.spotify.com/v1/users/${userId}/playlists`
+    await axios.post(
+      addPlaylistRoute,
+      {
+        name: playlistName,
+        description: playlistDescription,
+        public: true,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${shadowCredentials.access_token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    res.send('Successfully added playlist' + playlistName)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send('Error adding playlist')
+  }
+})
+
+export default router
