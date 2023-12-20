68ba9a7d410cb572499879c61093060dee41bd20
Murray Sinclair <msin981@gmail.com>
2023-12-21 11:45:54 +1300

    refactor: Server-Shadow  update endpoint client

--- client/apis/serverAdmin.ts
+++ client/apis/serverAdmin.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export function ShadowLogin () {
-  return window.location.href = '/api/v1/server-master/login'
+export function ShadowLogin() {
+  return (window.location.href = '/api/v1/shadow/login')
 }
 
-export function testShadowAccount () {
-  return axios.get('/api/v1/server-master/session/test')
+export function testShadowAccount() {
+  return axios.get('/api/v1/shadow/session/test')
 }
