c50c9e6512df6f7003285c798a7aa4e4dc8e3e15
Murray Sinclair <msin981@gmail.com>
2023-12-21 11:46:46 +1300

    refactor: Server add playlists endpoint

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import Shadow from './routes/spotifyLogin'
+import ShadowPlaylists from './routes/spotifyPlaylists'
 // import session from 'express-session'
 
 const server = express()
+server.use(express.json())
 
 // implement session and cookie to handle shadow account // server.use(session());
 
-server.use('/api/v1/server-master', Shadow)
+server.use('/api/v1/shadow', Shadow)
+server.use('/api/v1/playlists', ShadowPlaylists)
 
 export default server
