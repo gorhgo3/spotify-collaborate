9f044d4bff4c5ad358e3178c1519c8b48cf6fdb8
Murray Sinclair <msin981@gmail.com>
2023-12-20 15:33:29 +1300

    refactor: Update SpotifyRoutes for local variables- prep for session

--- server/routes/spotifyLogin.ts
+++ server/routes/spotifyLogin.ts
@@ -5,17 +5,19 @@ import generateRandomString from '../functions/randomString'
 import axios from 'axios'
 
 const router = Router()
-/*      /api/v1/server-master     */
-
 config()
+
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
 const redirect_uri =
-  process.env.SERVER_ADDRESS + '/api/v1/server-master/callback'
+process.env.SERVER_ADDRESS + '/api/v1/server-master/callback'
 
 
-// GET server account login
+//api/v1/server-master
+
+// GET server login
 router.get('/login', (req, res) => {
+  // Initialise server-spotify connection
   try {
     const state = generateRandomString(16)
     const scope = 'user-read-private user-read-email'
@@ -35,11 +37,18 @@ router.get('/login', (req, res) => {
   }
 })
 
-// GET server spotify callback route
+// update to session cookies
+let access_token
+let refresh_token
+let expires_in
+
+// GET server login callback
 router.get('/callback', async (req, res) => {
+  // Spotify authenticate callback
   const code = req.query.code || null
   const state = req.query.state || null
   if (state === null) {
+    // Failed
     res.redirect(
       '/#' +
         querystring.stringify({
@@ -47,6 +56,7 @@ router.get('/callback', async (req, res) => {
         })
     )
   } else {
+    // Success
     const response = await axios({
       method: 'post',
       url: 'https://accounts.spotify.com/api/token',
@@ -62,20 +72,19 @@ router.get('/callback', async (req, res) => {
           Buffer.from(client_id + ':' + client_secret).toString('base64'),
       },
     })
-    req.session.access_token = response?.data.access_token
-    req.session.refresh_token = response?.data.refresh_token
-    req.session.expires_in = response?.data.expires_in
-    req.session.save()
-    res.redirect('http://localhost:5173/')
+    access_token = response?.data.access_token
+    refresh_token = response?.data.refresh_token
+    expires_in = response?.data.expires_in
+    res.redirect(process.env.CLIENT_ADDRESS + '/home')
   }
 })
 
-// GET server test current session
+// GET shadow profile credentials (test)
 router.get('/session/test', async (req, res) => {
   try {
     const data = await axios
       .get('https://api.spotify.com/v1/me', {
-        headers: { Authorization: `Bearer ${req.session.access_token}` },
+        headers: { Authorization: `Bearer ${access_token}` },
       })
       .then((res) => res.data)
     res.json(data)
