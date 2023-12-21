957463edc6a9a804489d2e809b1264fb46e0634e
Murray Sinclair <msin981@gmail.com>
2023-12-21 21:49:07 +1300

    refactor: Including Playlists in HomePage.tsx

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import App from '@components/App'
 import Title from '@components/Title'
+import Playlists from '@components/Playlists'
 
 function HomePage() {
   return (
     <div>
       <Title />
       <App />
+      <Playlists />
     </div>
   )
 }
