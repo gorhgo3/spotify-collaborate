851ae18a9c4ec696b34d1863a35bfa41b076acab
Murray Sinclair <msin981@gmail.com>
2023-12-21 15:21:40 +1300

    fix: ShadowCredential Types

--- server/shadow.ts
+++ server/shadow.ts
@@ -3,14 +3,14 @@ import { UserDetails } from '../models/user'
 type ShadowCredentialsType = {
   access_token: string
   refresh_token: string
-  expires_in: string
+  expires_in: number
   user: UserDetails | null
 }
 
 const shadowCredentials: ShadowCredentialsType = {
   access_token: '',
   refresh_token: '',
-  expires_in: '',
+  expires_in: 0,
   user: null,
 }
 export default shadowCredentials
