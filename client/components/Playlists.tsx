2a74c2aa8635f014cf148387f13b7e17e069d274
Murray Sinclair <msin981@gmail.com>
2023-12-21 21:36:23 +1300

    feat: Playlists component added

---
+++ client/components/Playlists.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query'
+import { getPlaylists } from '../apis/playlists'
+import { Item } from '@models/playlists'
+import SinglePlaylist from '@components/SinglePlaylist'
+
+function Playlists() {
+  // need to include a mutation refetch
+  const { data, error } = useQuery({
+    // fetch all playlists of shadow account and display
+    queryKey: ['spotifyPlaylists'],
+    queryFn: async () => getPlaylists(),
+  })
+  
+
+  if (error) {
+    ;<div className="">
+      <h1>error</h1>
+    </div>
+  }
+
+  return (
+    <div className="container d-flex flex-wrap">
+      {data?.playlists.items.map((item: Item) => (
+        <SinglePlaylist {...item}/>
+      ))}
+    </div>
+  )
+}
+
+export default Playlists
+
