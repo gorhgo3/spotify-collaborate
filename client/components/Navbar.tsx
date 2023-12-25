5c4960eca09ec511b559d6af1e2600bf04e5afeb
Murray Sinclair <msin981@gmail.com>
2023-12-26 10:32:34 +1300

    feat: Navbar user details

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import useLogout from '../hooks/Logout'
+
 function Navbar() {
+  const { user } = useAuth0()
+  const handleBrowserLogout = useLogout()
+
   return (
     <div className="navbar">
       <a href="/home">Home</a>
-      <a href="/login">Login</a>
+      <div className="">
+        {user && <span>Hello, {user.nickname}</span>}
+        <button
+          className="btn btn-danger ml-4"
+          onClick={() => handleBrowserLogout()}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
