7fd214176d5f91f2bbb55981e207f144c2eb183c
Murray Sinclair <msin981@gmail.com>
2023-12-26 12:53:13 +1300

    feat: LoginPage added

---
+++ client/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import Header from '@components/Header.tsx'
+import Login from '@components/Login.tsx'
+
+function LoginPage() {
+  return (
+    <div className="container">
+      <Header />
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
