16689db207ddc56d738f7d3fbc9c4d71964b08f4
Murray Sinclair <msin981@gmail.com>
2023-12-26 12:54:19 +1300

    refactor: HomePage update to latest components and links

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,13 +1,37 @@
-import App from '@components/App'
-import Title from '@components/Title'
-import Playlists from '@components/Playlists'
+import Header from '@components/Header'
+import Notice from '@components/Notice'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
     <div>
-      <Title />
-      <App />
-      <Playlists />
+      <Header />
+      <div className="container text-center">
+        <div className="container d-flex my-5">
+          <Link
+            to="/playlists"
+            className="nav-selectors"
+            style={{ backgroundColor: '#1ED760' }}
+          >
+            Playlists
+          </Link>
+          <Link
+            to="/playlists"
+            className="nav-selectors"
+            style={{ backgroundColor: '#FFFFFF' }}
+          >
+            Profile
+          </Link>
+          <Link
+            to="/playlists"
+            className="nav-selectors"
+            style={{ backgroundColor: '#191414', color: 'white' }}
+          >
+            Tracks
+          </Link>
+        </div>
+      </div>
+      <Notice />
     </div>
   )
 }
