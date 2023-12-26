092ee4209ab1c4f8b92f2619a2845293e682d0bc
Murray Sinclair <msin981@gmail.com>
2023-12-26 21:14:12 +1300

    feat: Client routes includes PlaylistsPage and PlaylistDetails

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -1,6 +1,8 @@
 import Login from '@components/Login'
 import HomePage from '../pages/HomePage'
 import UserPage from '../pages/UserPage'
+import PlaylistsPage from '../pages/PlaylistsPage'
+import PlaylistDetails from '../pages/PlaylistDetails'
 import { createRoutesFromElements, Route } from 'react-router-dom'
 
 export const router = createRoutesFromElements(
@@ -9,6 +11,8 @@ export const router = createRoutesFromElements(
       <Route path="home" element={<HomePage />} />
       <Route path="login" element={<Login />} />
       <Route path="profile" element={<UserPage />} />
+      <Route path="playlists" element={<PlaylistsPage />} />
+      <Route path="playlists/:playlistId" element={<PlaylistDetails />} />
     </Route>
   </>
 )
