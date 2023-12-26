db941cde7db2d07222c2e98fd16287b521c60ea7
Murray Sinclair <msin981@gmail.com>
2023-12-26 21:12:36 +1300

    refactor: Wrap SinglePlaylist in Link

--- client/components/SinglePlaylist.tsx
+++ client/components/SinglePlaylist.tsx
@@ -1,23 +1,29 @@
 import { Item } from '@models/playlists'
+import { Link } from 'react-router-dom'
+import React from 'react'
 
 function SinglePlaylist(props: Item) {
   return (
     <div className="card m-1" style={{ width: '15rem' }}>
-      <img
-        className="card-img-top"
-        src={props.images[0]?.url}
-        alt="Card image cap"
-      />
-      <div className="card-body d-flex flex-column justify-content-between">
-        <h5 className="card-title text-truncate">{props.name}</h5>
-        <ul className="font-italic px-0" style={{ fontSize: '0.5rem' }}>
-          {props.id}
-        </ul>
-        <p className="card-text text-truncate">{props.description}</p>
-        <a href="#" className="btn btn-primary">
+      <Link
+        to={props.id}
+        className=""
+        style={{ textDecoration: 'none', color: 'black' }}
+      >
+        <img
+          className="card-img-top"
+          src={props.images[0]?.url}
+          alt="Card image cap"
+        />
+        <div className="card-body d-flex flex-column justify-content-between">
+          <h5 className="card-title text-truncate">{props.name}</h5>
+          <ul className="font-italic px-0" style={{ fontSize: '0.5rem' }}>
+            {props.id}
+          </ul>
+          <p className="card-text text-truncate">{props.description}</p>
           View details
-        </a>
-      </div>
+        </div>
+      </Link>
     </div>
   )
 }
