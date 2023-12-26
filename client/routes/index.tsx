3df85c9ab3d5c491754e0692e9e0fb0ff639ec11
Murray Sinclair <msin981@gmail.com>
2023-12-26 15:48:25 +1300

    feat: New Route /profile

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -1,5 +1,6 @@
 import Login from '@components/Login'
 import HomePage from '../pages/HomePage'
+import UserPage from '../pages/UserPage'
 import { createRoutesFromElements, Route } from 'react-router-dom'
 
 export const router = createRoutesFromElements(
@@ -7,6 +8,7 @@ export const router = createRoutesFromElements(
     <Route path="/">
       <Route path="home" element={<HomePage />} />
       <Route path="login" element={<Login />} />
+      <Route path="profile" element={<UserPage />} />
     </Route>
   </>
 )
