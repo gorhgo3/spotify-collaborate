60480573ed8fc4ec0c1e1bc64e5c70f2ceff112d
Murray Sinclair <msin981@gmail.com>
2023-12-27 12:53:33 +1300

    feat: Client Apis to addPlaylistTracks && checkSpotifyTracks

--- client/apis/playlists.ts
+++ client/apis/playlists.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import Playlist from '@models/playlists'
+import TracksResults from '@models/trackResults'
 
 type Playlists = {
   playlists: Playlist
-}
+} 
 
 export function getPlaylists(): Promise<Playlists> {
   return axios.get('/api/v1/playlists/get-playlists').then((res) => res.data)
@@ -11,4 +12,18 @@ export function getPlaylists(): Promise<Playlists> {
 
 export function getPlaylistDetails(playlistId: string): Promise<Playlist> {
   return axios.get(`/api/v1/playlists/details/${playlistId}`)
+}
+
+export function addPlaylistTrack(playlistId: string, trackUri: string): any {
+  console.log(trackUri, playlistId);
+
+  return axios.post(`/api/v1/playlists/add-track/${trackUri}`, {
+    data: {
+      playlistId,
+    },
+  })
+}
+
+export function checkSpotifyTracks(search: string): Promise<TracksResults> {
+  return axios.get(`/api/v1/playlists/search/${search}`)
 }
