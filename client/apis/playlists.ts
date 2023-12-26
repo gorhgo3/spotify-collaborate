26ac05c2f538cb5441a47a1943e72b20297dfb9e
Murray Sinclair <msin981@gmail.com>
2023-12-26 21:11:46 +1300

    feat: Client api getPlaylistDetails

--- client/apis/playlists.ts
+++ client/apis/playlists.ts
@@ -8,3 +8,7 @@ type Playlists = {
 export function getPlaylists(): Promise<Playlists> {
   return axios.get('/api/v1/playlists/get-playlists').then((res) => res.data)
 }
+
+export function getPlaylistDetails(playlistId: string): Promise<Playlist> {
+  return axios.get(`/api/v1/playlists/details/${playlistId}`)
+}
