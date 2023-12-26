d1c7b10952c1326eedaff061ee7c507860050a9f
Murray Sinclair <msin981@gmail.com>
2023-12-27 12:52:27 +1300

    feat: PlaylistDetails handles playlist rendering

--- client/pages/PlaylistDetails.tsx
+++ client/pages/PlaylistDetails.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
-import { getPlaylistDetails } from '../apis/playlists'
+import {
+  addPlaylistTrack,
+  checkSpotifyTracks,
+  getPlaylistDetails,
+} from '../apis/playlists'
 import { Playlist, Item } from '@models/playlist'
+import NewTrack from '@components/NewTrack'
 
 function PlaylistDetails() {
   const { playlistId } = useParams()
@@ -20,16 +25,50 @@ function PlaylistDetails() {
   if (isLoading) return <p>Collecting playlist..</p>
   if (data)
     return (
-      <div>
-        <h4>PlaylistDetails</h4>
-        <h4>{data.name}</h4>
-        <img src={data.images[0]?.url} alt="" />
-        <p>{data.description}</p>
-        {data.tracks.items.map((item: Item) => (
-          <div className="track" key={item.track.id}>
-            <p>{item.track.album.name}</p>
+      <div className="container justify-content-center text-center">
+        <div className="playlist">
+          {/* playlist header */}
+          <div className="playlist-header">
+            <h1 style={{ color: '#1ED760' }}>Playlist</h1>
+            <h4 className="playlist-title title">{data.name}</h4>
+            <img
+              className="playlist-image"
+              src={data.images[0]?.url}
+              alt="playlist-image"
+            />
+            <p className="playlist-description">{data.description}</p>
+            <div className="addToPlaylist">
+              {/* playlist add track form */}
+              <NewTrack id={playlistId as string} />
+            </div>
           </div>
-        ))}
+          {/* playlist tracks */}
+          {data.tracks.items.map((item: Item) => (
+            <div className="track" key={item.track.id}>
+              <div className="details d-flex">
+                <img
+                  src={item.track.album.images[0]?.url}
+                  alt=""
+                  style={{ width: '3rem', height: '3rem' }}
+                  className="mr-2"
+                />
+                <div className="details-text text-left">
+                  <p className="mb-0">{item.track.name}</p>
+                  <p className="mb-0" style={{ color: 'grey' }}>
+                    {item.track.explicit && '🅴'}
+                    {item.track.artists[0].name}
+                  </p>
+                </div>
+                <div className="ml-auto mr-2 align-self-center">
+                  <i
+                    className="fa-regular fa-circle-play"
+                    style={{ fontSize: '1.5rem', color: '#1ED760' }}
+                  ></i>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     )
 }
