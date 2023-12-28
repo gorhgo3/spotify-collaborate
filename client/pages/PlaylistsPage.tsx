97e861174e7ac23da1b6ecb8ed5d455e965e8d9e
Murray Sinclair <msin981@gmail.com>
2023-12-28 14:15:10 +1300

    refactor: PlaylistPage add spotify logo and disclaimers

--- client/pages/PlaylistsPage.tsx
+++ client/pages/PlaylistsPage.tsx
@@ -3,18 +3,19 @@ import Playlists from '@components/Playlists'
 
 function PlaylistsPage() {
   return (
-    <div>
-      <h4>This is the playlists page</h4>
-      <p>
-        We need to display all the playlists, and then allow the current user to
-        add to the collaborated playlists. All playlists are linked to the
-        server account, and will be displayed and be customisable.
-      </p>
-      <p>
-        <b>
-          Displaying all the shadow account playlists that you have access to
-          contribute to.
-        </b>
+    <div className="playlists-container container text-center">
+      <h1 className="title p-5">Spotify Collab library</h1>
+      <img
+        src="client/assets/Spotify-logo.png"
+        alt=""
+        className="img-fluid mx-auto d-block"
+        style={{ width: '15rem' }}
+      />
+      <p className="m-5">
+        <b>Developers disclaimer** </b>
+        <br />
+        All playlists are from server account- collaborate via authenticated
+        client account.
       </p>
       <Playlists />
     </div>
